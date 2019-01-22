ccd57b14e2c84964ff04e343dffd906ca759210b
Simon Coffey <simon@urbanautomaton.com>
2019-01-22 08:56:56 +0000

    More normalisation and whitespace handling

--- index.rb
+++ index.rb
@@ -1,17 +1,40 @@
 require 'combine_pdf'
 require 'yomu'
 
-terms = File.foreach("terms.txt").map(&:chomp).map(&:downcase).map { |t| t.squeeze(' ') }
+def normalize(text)
+  text
+    .strip
+    .downcase
+    .gsub(/\s+/, ' ')
+end
+
+def term_whitespace_combos(term)
+  tokens = term.split(/\b/)
+  output = tokens.take(1)
+  tokens.drop(1).each do |t|
+    if t == " "
+      output = output + output.map { |o| o + t }
+    else
+      output.each { |o| o << t }
+    end
+  end
+  output
+end
+
+terms = File.foreach("terms.txt").map(&method(:normalize))
 term_pages = Hash.new { |h, k| h[k] = [] }
 
 pdf = CombinePDF.load("source.pdf")
-pdf.pages.take(20).each_with_index do |page, i|
+pdf.pages.each_with_index do |page, i|
   $stdout.write "Page #{i}\r"
+
   p = CombinePDF.new
   p << page
-  text = Yomu.read(:text, p.to_pdf).downcase.gsub(/\s/, ' ').squeeze(' ')
+
+  text = normalize(Yomu.read(:text, p.to_pdf))
+
   terms.each do |term|
-    if text.include?(term)
+    if term_whitespace_combos(term).any? { |c| text.include?(c) }
       term_pages[term] << i + 1
     end
   end
